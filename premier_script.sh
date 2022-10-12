808fe580771386949509c5d76ab9511a79047346
Kexin <guikexinfr@outlook.com>
2022-10-12 11:17:03 +0200

    ajout d'un script pour compter des annotations

---
+++ premier_script.sh
@@ -0,0 +1,37 @@
+
+echo "pour l'année 2016 > sortie.txt
+grep "Location" 2016*.ann | ec -l >>sortie.txt
+echo "pour l'année 2017" >>sortie.txt
+grep "Location" 2017*.ann | wc -l >> sortie.txt
+echo "pour l'année 2018" >> sortie.txt
+grep "Location" 2018*.ann |wc -l >> sortie.txt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
